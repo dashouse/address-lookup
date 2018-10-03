7e1d40786381711ebd47abad50857d1cf32985b9
Dave House <david.house@digital.cabinet-office.gov.uk>
2018-10-03 15:24:38 +0100

    Add example address lookups

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,4 +7,55 @@ if (window.console && window.console.info) {
 
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
+
+  const postcodes = [
+    '1 Farnell Road, Staines, TW184HT',
+    '2 Farnell Road, Staines, TW184HT',
+    '3 Farnell Road, Staines, TW184HT',
+    '4 Farnell Road, Staines, TW184HT',
+    '5 Farnell Road, Staines, TW184HT',
+    '6 Farnell Road, Staines, TW184HT',
+    '7 Farnell Road, Staines, TW184HT',
+    '8 Farnell Road, Staines, TW184HT',
+    '9 Farnell Road, Staines, TW184HT',
+    '10 Farnell Road, Staines, TW184HT',
+    '11 Farnell Road, Staines, TW184HT',
+    '12 Farnell Road, Staines, TW184HT',
+    '13 Farnell Road, Staines, TW184HT',
+    '14 Farnell Road, Staines, TW184HT',
+    '15 Farnell Road, Staines, TW184HT',
+    '16 Farnell Road, Staines, TW184HT',
+    '17 Farnell Road, Staines, TW184HT',
+    '18 Farnell Road, Staines, TW184HT',
+    '19 Farnell Road, Staines, TW184HT',
+    '20 Farnell Road, Staines, TW184HT',
+    '21 Farnell Road, Staines, TW184HT',
+    '22 Farnell Road, Staines, TW184HT',
+    '23 Farnell Road, Staines, TW184HT',
+    '24 Farnell Road, Staines, TW184HT',
+    '25 Farnell Road, Staines, TW184HT',
+    '26 Farnell Road, Staines, TW184HT',
+    '27 Farnell Road, Staines, TW184HT',
+    '28 Farnell Road, Staines, TW184HT',
+    '29 Farnell Road, Staines, TW184HT',
+    '30 Farnell Road, Staines, TW184HT',
+    '31 Farnell Road, Staines, TW184HT',
+    '32 Farnell Road, Staines, TW184HT',
+    '33 Farnell Road, Staines, TW184HT',
+    '34 Farnell Road, Staines, TW184HT',
+    '35 Farnell Road, Staines, TW184HT',
+    '36 Farnell Road, Staines, TW184HT',
+    '37 Farnell Road, Staines, TW184HT',
+    '38 Farnell Road, Staines, TW184HT',
+    '39 Farnell Road, Staines, TW184HT',
+    '40 Farnell Road, Staines, TW184HT',
+    '41 Farnell Road, Staines, TW184HT'
+  ]
+
+  accessibleAutocomplete({
+    element: document.querySelector('#my-autocomplete-container'),
+    id: 'my-autocomplete', // To match it to the existing <label>.
+    source: postcodes
+  })
+
 })
--- app/routes.js
+++ app/routes.js
@@ -4,3 +4,23 @@ const router = express.Router()
 // Add your routes here - above the module.exports line
 
 module.exports = router
+
+// Route index page
+router.get('/', function (req, res) {
+  res.render('index')
+})
+
+
+router.post('/single-postcode-input-step-2', function (req, res) {
+  // make a variable and give it the value from 'juggling-balls'
+  var validPostCode = req.session.data['postcode'].toLowerCase().replace(/\s+/g,'')
+
+  //check if the variable matches a condition
+  if (validPostCode == "tw184ht") {
+    // send user to the next page
+    res.redirect('/single-postcode-input-step-2')
+  } else {
+    // send user to can't find address
+    res.redirect('/single-postcode-input-cant-find-address')
+  }
+})
