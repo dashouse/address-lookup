3e44822501d2902ecfbcd050747af95db07e9498
Dave House <david.house@digital.cabinet-office.gov.uk>
2018-10-09 09:26:22 +0100

    Add google and home office variants

--- app/routes.js
+++ app/routes.js
@@ -24,3 +24,18 @@ router.post('/single-postcode-input-step-2', function (req, res) {
     res.redirect('/single-postcode-input-cant-find-address')
   }
 })
+
+
+router.post('/single-postcode-input-HO-step-2', function (req, res) {
+  // make a variable and give it the value from 'juggling-balls'
+  var validPostCode = req.session.data['postcode'].toLowerCase().replace(/\s+/g,'')
+
+  //check if the variable matches a condition
+  if (validPostCode == "tw184ht") {
+    // send user to the next page
+    res.redirect('/single-postcode-input-HO-step-2')
+  } else {
+    // send user to can't find address
+    res.redirect('/single-postcode-input-HO-cant-find-address')
+  }
+})
